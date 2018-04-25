dbfcd1965bf8d6d59fc3083dd8f4126221bd299c
Oskari Sieranen <oskari.sieranen@gmail.com>
2018-04-25 14:25:08 +0300

    Added the opening speech, and basic commmand structure

--- CallingOfVilla.py
+++ CallingOfVilla.py
@@ -1,34 +1,44 @@
 # Tabs: 4 spaces
 
 import mysql.connector
+import time
 
 # Commands go here | First person always. Ex. "I have *List of items*"
 
 def inventory():
     cur = db.cursor()
-    sql = "SELECT ObjectId, Description FROM Object"
+    sql = "SELECT ObjectId, Description FROM Object WHERE Location = Player;"
     cur.execute(sql)
     if cur.rowcount()>=1:
         print("I have: ")
         for item in cur.fetchall():
-            print("-" + row[0])
+            print("-" + item[0])
     else:
         print("I don't have anything at the moment.")
     return
 
 # Connection here
 db = mysql.connector.connect(host="localhost",
-                            user="",
-                            passwd="",
-                            db="CallingOfVilla",
-                            buffered=True)
+                           user="",
+                           passwd="",
+                           db="CallingOfVilla",
+                           buffered=True)
 
                             
 # Initializing the location and an empty screen
+print("\n"*1000)
+PlayerName = input("Give me your name: ")
+print("Your name is",PlayerName,"and you are an aspiring blogger going to explore and old abandoned mansion to record your journey for money and fame.")
+print("Your friend dropped of you at the edge of the forest and after a couple of hours of hiking, you have finally arrived at the old mansion.")
+print("You enter the once magnificent building and hear the giant double doors lock behind your back.")
+print("The old house seems creepier and creepier by the second, and you have to...")
+time.sleep(20)
+print("... G E T  O U T...")
+location = "MAINHALL"
+command = ""
 
 # Main Loop
-while command != "quit" and location !=  : # Name of end screen
-
+while command != "quit" and location != "END":
     print("")
     operation = input("What to do: ").split()
     # The user's command is taken
@@ -43,3 +53,15 @@ while command != "quit" and location !=  : # Name of end screen
         target = ""
     # Print an empty line for readability
     print("")
+    #All of the action come here vvv
+    if command=="inventory" or command=="i":
+        inventory()
+    elif command!="quit" and command!="":
+        print("I don't think I can",command)
+
+
+if (location=="EXIT"):
+    print("") # Victory speech here
+else:
+    print("Don't be gone too long...")
+db.rollback()
